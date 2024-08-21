472e57a245d73aec218528084dfce167811a583f
Aleksandar Bursać <aleksandarbursac150699@gmail.com>
2024-08-21 18:17:26 +0200

    Creating login and registration page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+
 
 function App() {
   return (
+    
     <div className="App">
           <Router>
+            <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+              <div className='container-fluid'>
+                <Link className='navbar-brand' to='/'>BURSAC IT SHOP</Link>
+                
+                <div className='collapse navbar-collapse' id='navbarNav'>
+                  <ul className='navbar-nav'>
+                  <li className='nav-item'>
+                    <Link className='nav-link' to='/'>Home</Link>
+                    </li>
+                    <li className='nav-item'>
+                    <Link className='nav-link' to='/contact'>Contact</Link>
+                    </li>
+                    <li className='nav-item'>
+                    <Link className='nav-link' to='/about'>About</Link>
+
+                    </li>
+                    <li className='nav-item'>
+                    <Link className='nav-link' to='/login'>Login</Link>
+
+                    </li>
+                    <li className='nav-item'>
+                    <Link className='nav-link' to='/registration'>Registration</Link>
+
+                    </li>
+
+                  </ul>
+
+                </div>
+              </div>
+              
+            </nav>
             <Routes>
               <Route path='/' exact Component={Home}/>
               <Route path='/contact' exact Component={Contact}/>
               <Route path='/about' exact Component={About}/>
+              <Route path='/login' exact Component={Login}/>
+              <Route path='/registration' exact Component={Registration}/>
             </Routes>
           </Router>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function Home() {
     return(
-        <div className="App">
-            HOME PAGE
+        <div className="container text-center mt-3 col-lg-4 col-md-6 col-sm-12">
+            <h1 className="display-4">HOME PAGE</h1>
+            <p className="lead">This is the contact page of our website.</p>
     </div>
     )
     
