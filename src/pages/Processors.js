10a3e172eb6d0bee3b8ccf8e7168f26b4402c4cb
Aleksandar Bursać <aleksandarbursac150699@gmail.com>
2024-08-26 20:29:46 +0200

    - Added UI elements and functionality for updating processor details. - Implemented delete functionality allowing users to remove processors. - Ensured seamless interaction with the backend for update and delete operations.

--- src/pages/Processors.js
+++ src/pages/Processors.js
@@ -24,12 +24,12 @@ const Processors = () => {
             <ul>
                 {processors.map((processor) => (
                     <li key={processor.id}>
-                        <h2>{processor.name}</h2>
+                        <h2>{processor.model}</h2>
                         <p>Cores: {processor.cores}</p>
                         <p>Threads: {processor.threads}</p>
                         <p>Base Clock: {processor.baseClock} GHz</p>
                         <p>Boost Clock: {processor.boostClock} GHz</p>
-                        <p>Price: ${processor.price}</p>
+                        <p>Price: {processor.price} din</p>
                         
                         {processor.imageUrl && <img className="imageSize" src={`http://localhost:3001/${processor.imageUrl}`} alt={`${processor.name} ${processor.model}`} /> }
                         
