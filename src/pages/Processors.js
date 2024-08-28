377cc01ceec9c98d8c3dc2bd8326feb194ede4c9
Aleksandar Bursać <aleksandarbursac150699@gmail.com>
2024-08-28 15:48:41 +0200

    Just adding new components pages like GPU, RAM, PSU and others..

--- src/pages/Processors.js
+++ src/pages/Processors.js
@@ -19,26 +19,29 @@ const Processors = () => {
     }, []);
 
     return(
-        <div>
-            <h1>Processors</h1>
-            <ul>
-                {processors.map((processor) => (
-                    <li key={processor.id}>
-                        <h2>{processor.model}</h2>
-                        <p>Cores: {processor.cores}</p>
-                        <p>Threads: {processor.threads}</p>
-                        <p>Base Clock: {processor.baseClock} GHz</p>
-                        <p>Boost Clock: {processor.boostClock} GHz</p>
-                        <p>Price: {processor.price} din</p>
-                        
-                        {processor.imageUrl && <img className="imageSize" src={`http://localhost:3001/${processor.imageUrl}`} alt={`${processor.name} ${processor.model}`} /> }
-                        
-                        
-                        
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <div className="container">
+    <h1>Processors</h1>
+    <div className="row">
+        {processors.map((processor) => (
+            <div key={processor.id} className="col-md-4 mb-4">
+                <div className="card">
+                    <div >
+                    {processor.imageUrl && <img className="card-img-top img-fluid " src={`http://localhost:3001/${processor.imageUrl}`} alt={`${processor.name} ${processor.model}`} />}
+                        </div>
+                    
+                    <div className="card-body">
+                        <h5 className="card-title">{processor.model}</h5>
+                        <p className="card-text">Cores: {processor.cores}</p>
+                        <p className="card-text">Threads: {processor.threads}</p>
+                        <p className="card-text">Base Clock: {processor.baseClock} GHz</p>
+                        <p className="card-text">Boost Clock: {processor.boostClock} GHz</p>
+                        <p className="card-text">Price: {processor.price} din</p>
+                    </div>
+                </div>
+            </div>
+        ))}
+    </div>
+</div>
     )
 }
 
