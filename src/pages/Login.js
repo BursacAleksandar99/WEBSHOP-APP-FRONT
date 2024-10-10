6febe7a192dd946573c8517107c18f4dfdb43a82
Aleksandar Bursać <aleksandarbursac150699@gmail.com>
2024-10-10 19:46:33 +0200

    Upgrade for login page, almost done!

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -28,34 +28,60 @@ function Login(){
     }
 
     return(
-        <div className="mt-5 container">
-      <h1>Login Page</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Enter your username"
-            required
-          />
+    //     <div className="mt-5 container">
+    //   <h1>Login Page</h1>
+    //   <form onSubmit={handleLogin}>
+    //     <div>
+    //       <label htmlFor="username">Username:</label>
+    //       <input
+    //         type="text"
+    //         id="username"
+    //         value={username}
+    //         onChange={(e) => setUsername(e.target.value)}
+    //         placeholder="Enter your username"
+    //         required
+    //       />
+    //     </div>
+    //     <div>
+    //       <label htmlFor="password">Password:</label>
+    //       <input
+    //         type="password"
+    //         id="password"
+    //         value={password}
+    //         onChange={(e) => setPassword(e.target.value)}
+    //         placeholder="Enter your password"
+    //         required
+    //       />
+    //     </div>
+    //     <button type="submit">Login</button>
+    //   </form>
+    //   {error && <p style={{ color: 'red' }}>{error}</p>}
+    // </div>
+    <div className=" login-div0 col-12 row">
+
+      <div className="login-div1 col-lg-6 col-sm-12 ">
+        
+        <div className="login-div1-2">
+            <p>Do you want a new gaming computer or 
+              are you looking to take your business to the next level?</p>
+              <p>You're in the right place!</p>
+          </div>
+        <div className="login-div1-1">
+          <img src="https://res.cloudinary.com/djdzumd6m/image/upload/v1728577955/DALL_E_2024-10-10_18.29.47_-_A_minimalist_illustration_of_a_man_sitting_at_a_computer_desk_playing_video_games_with_a_dark_gray_343A40_background_and_the_man_in_pure_white_-fotor-bg-remover-20241010183134_jlsnic.png" alt="guy plaing games xD"/>
+
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter your password"
-            required
-          />
+        <div className="login-div-1-3">
+            <h1>WELCOME BACK TO BURSAC IT SHOP!</h1>
         </div>
-        <button type="submit">Login</button>
-      </form>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+        
+      </div>
+
+
+      
+      <div className="login-div2 col-lg-6 col-sm-12 ">
+      
+      </div>
+
     </div>
     );
 }
